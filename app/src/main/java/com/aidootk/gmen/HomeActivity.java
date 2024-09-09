e8adf7d986d9b02e4150fa2bbbb2c96e0aca1308
thilong <thilong@126.com>
2024-09-09 21:36:59 +0800

    init.

---
+++ app/src/main/java/com/aidootk/gmen/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.aidootk.gmen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.aidootk.gmen.droid.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
